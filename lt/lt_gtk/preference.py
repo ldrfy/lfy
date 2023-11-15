fc0a39c69dc6952086f66fd5267c0f86938ce175
yuhldr <yuhldr@qq.com>
2023-11-15 13:32:15 +0800

    init

---
+++ lt/lt_gtk/preference.py
@@ -0,0 +1,13 @@
+# window.py
+#
+# Copyright 2023 Unknown
+
+from gi.repository import Adw, Gtk
+
+
+@Gtk.Template(resource_path='/lt/ldr/cool/ui/preference.ui')
+class PreferenceWindow(Adw.PreferencesWindow):
+    __gtype_name__ = 'PreferencesWindow'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
