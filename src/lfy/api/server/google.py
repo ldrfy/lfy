54dcd02ad721ef3e5e3d269584fff8218c0f7ea3
yuhldr <yuhldr@qq.com>
2023-11-17 01:19:43 +0800

    chore: 细节提示

--- src/lfy/api/server/google.py
+++ src/lfy/api/server/google.py
@@ -1,17 +1,29 @@
-import json
+"""谷歌翻译
 
+Returns:
+    _type_: _description_
+"""
 import requests
 
 from lfy.api.server import TIME_OUT
 
 
 def translate_text(text, to_lang_code="zh-cn", from_lang="auto"):
+    """翻译
+
+    Args:
+        text (str): 待翻译字符
+        to_lang_code (str, optional): 翻译成什么语言. Defaults to "zh-cn".
+        from_lang (str, optional): 文本是什么语言. Defaults to "auto".
+
+    Returns:
+        str: _description_
+    """
     text = text.replace("#", "")
-    print("谷歌" + to_lang_code)
     url = 'https://translate.googleapis.com/translate_a/single?'
     param = f'client=gtx&dt=t&sl={from_lang}&tl={to_lang_code}&q={text}'
     response = requests.get(url + param, timeout=TIME_OUT)
-    result = json.loads(response.text)
+    result = response.json()
 
     s = ""
     for ss in result[0]:
