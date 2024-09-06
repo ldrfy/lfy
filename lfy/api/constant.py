4803060c2463e926eb4a3ed3bee224036cd3eacb
yuhldr <yuhldr@qq.com>
2024-09-06 22:39:47 +0800

    fix: 兼容python3.6

--- lfy/api/constant.py
+++ lfy/api/constant.py
@@ -1,15 +1,20 @@
 '常量，尽可能不引用其他文件'
-from lfy.api.server import (Server, aliyun, baidu, bing, com, google, huoshan,
-                            tencent)
+from lfy.api.server.aliyun import AliYunServer
+from lfy.api.server.baidu import BaiduServer
+from lfy.api.server.bing import BingServer
+from lfy.api.server.com import AllServer
+from lfy.api.server.google import GoogleServer
+from lfy.api.server.huoshan import HuoShanServer
+from lfy.api.server.tencent import TencentServer
 
-SERVERS: list[Server] = [
-    com.AllServer(),
-    google.GoogleServer(),
-    bing.BingServer(),
-    baidu.BaiduServer(),
-    tencent.TencentServer(),
-    aliyun.AliYunServer(),
-    huoshan.HuoShanServer(),
+SERVERS = [
+    AllServer(),
+    GoogleServer(),
+    BingServer(),
+    BaiduServer(),
+    TencentServer(),
+    AliYunServer(),
+    HuoShanServer(),
 ]
 
 
