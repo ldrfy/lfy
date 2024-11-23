0c6541c0522236ccd858091c5e3194f8fb074d03
yuhldr <yuhldr@qq.com>
2024-11-23 11:58:07 +0800

    fix: 防止复制时翻译

--- lfy/qt/__init__.py
+++ lfy/qt/__init__.py
@@ -1,6 +1,26 @@
 import time
 
-from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
+from PyQt6.QtWidgets import QPlainTextEdit
+
+
+class MyPlainTextEdit(QPlainTextEdit):
+    """禁止复制响应
+
+    Args:
+        QPlainTextEdit (_type_): _description_
+    """
+    def keyPressEvent(self, event):
+        """内部的ctrl c不要响应
+
+        Args:
+            event (_type_): _description_
+        """
+        if event.modifiers() == Qt.KeyboardModifier.ControlModifier and event.key() == Qt.Key.Key_C:
+            print("no copy")
+            event.accept()  # 阻止事件传播，防止触发 QClipboard 的复制
+        else:
+            super().keyPressEvent(event)
 
 
 class MyThread(QThread):
@@ -10,9 +30,6 @@ class MyThread(QThread):
         self.fun = fun
         self.param = param
         super().__init__()
-
-    def __del__(self):
-        self.wait()
 
     def run(self):
         start_ = time.time()
