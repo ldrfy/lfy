d8a0a7cd169ed4acc129eb8463d79799141f3bc0
yuhldr <yuhldr@qq.com>
2024-11-23 21:18:21 +0800

    fix: 无需修正lib

--- lfy/gtk/lfy.py
+++ lfy/gtk/lfy.py
@@ -6,11 +6,6 @@ APP_NAME = '@APP_NAME@'
 PYTHON_DIR = '@PYTHON_DIR@'
 
 print("PYTHON_DIR", PYTHON_DIR)
-if not os.path.exists(f"{PYTHON_DIR}/{APP_NAME}"):
-    print("修正")
-    THIS_DIR, THIS_FILENAME = os.path.split(__file__)
-    PYTHON_DIR = os.path.abspath(f"{THIS_DIR}/../lib/")
-    print(PYTHON_DIR)
 sys.path.append(PYTHON_DIR)
 
 from lfy.gtk.main import main  # pylint: disable=C0413
