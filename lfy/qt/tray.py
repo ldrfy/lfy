f9b4ace4d299a7cc317222cc1f4124289edb12ae
yuhldr <yuhldr@qq.com>
2024-11-25 12:34:21 +0800

    feat: qt 查找更新

--- lfy/qt/tray.py
+++ lfy/qt/tray.py
@@ -1,13 +1,16 @@
 '托盘图标'
+import time
 from gettext import gettext as _
 
 from PyQt6.QtGui import QAction, QClipboard
 from PyQt6.QtWidgets import QApplication, QMenu, QMessageBox, QSystemTrayIcon
 
 from lfy import APP_NAME, PACKAGE_URL, PACKAGE_URL_BUG, VERSION
+from lfy.qt import MyThread
 from lfy.qt.preference import PreferenceWindow
 from lfy.qt.translate import TranslateWindow
 from lfy.utils import cal_md5
+from lfy.utils.check_update import main as check_update
 from lfy.utils.settings import Settings
 
 
@@ -24,6 +27,7 @@ class TrayIcon(QSystemTrayIcon):
         self.app: QApplication = app
         self.setToolTip(APP_NAME)
         self.sg = Settings()
+        self.cb: QClipboard = self.app.clipboard()
 
         # 创建托盘菜单
         tray_menu = QMenu(parent)
@@ -31,7 +35,9 @@ class TrayIcon(QSystemTrayIcon):
         open_action.triggered.connect(self.w.show)
         tray_menu.addAction(open_action)
 
-        self.cb: QClipboard = self.app.clipboard()
+        fu_action = QAction(_("Check for updates"), self)
+        fu_action.triggered.connect(self.find_update)
+        tray_menu.addAction(fu_action)
 
         self.auto_action = QAction(
             _('Copy to translate'), triggered=self.copy2translate)
@@ -40,7 +46,6 @@ class TrayIcon(QSystemTrayIcon):
         self.auto_action.setChecked(
             self.sg.g("copy-auto-translate", d=True, t=bool))
         self.copy2translate()
-
         tray_menu.addAction(self.auto_action)
 
         pf_action = QAction(_("Preference"), self)
@@ -59,6 +64,8 @@ class TrayIcon(QSystemTrayIcon):
 
         self.img_md5 = ""
         self.text_last = ""
+        self.my_thread = None
+        self.find_update()
 
     def _on_clipboard_data_changed(self):
 
@@ -137,4 +144,44 @@ class TrayIcon(QSystemTrayIcon):
             t = _("Stop copy to translate")
             m = _("Copy detected, not automatically translated")
             n = QSystemTrayIcon.MessageIcon.Critical
+
         self.showMessage(t, m, n, 2000)
+
+    def update_app(self, p):
+        """显示更新信息
+
+        Args:
+            update_msg (_type_): _description_
+        """
+        t, update_msg, n = p
+        if update_msg is None:
+            return
+        # pylint: disable=E1101
+        self.my_thread.clean_up()
+        self.w.translate_text(update_msg)
+        self.showMessage(t, update_msg, n, 2000)
+
+    def find_update(self, auto=True):
+        """查找更新
+        """
+
+        def fu(_p=None):
+            """更新子线程
+            """
+            update_msg = check_update()
+            if update_msg is not None:
+                time.sleep(2)
+                return (_("New version available!"), update_msg, QSystemTrayIcon.MessageIcon.Warning)
+
+            if not auto:
+                # 手动更新
+                s = _("There is no new version.\
+                      \nThe current version is {}.\
+                      \nYou can go to {} to view the beta version.")\
+                      .format(VERSION, PACKAGE_URL)
+                return (_("No new version!"), s, QSystemTrayIcon.MessageIcon.Critical)
+            return (None, None, None)
+        if self.sg.g("auto-check-update"):
+            self.my_thread = MyThread(fu)
+            self.my_thread.signal.connect(self.update_app)
+            self.my_thread.start()
