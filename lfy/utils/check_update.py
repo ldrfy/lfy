7ddfd271a1282b85e0b61f239a61770bf44fdff1
yuhldr <yuhldr@qq.com>
2024-12-08 13:17:59 +0800

    fix: 更新提示不要因为网络错误提示

--- lfy/utils/check_update.py
+++ lfy/utils/check_update.py
@@ -9,6 +9,9 @@ from lfy import PACKAGE_URL, VERSION  # pylint: disable=E0611
 from lfy.api.server import TIME_OUT
 from lfy.utils.debug import get_logger
 
+MSG_UPDATE_ERROR = "Please update. There is a problem with the current version configuration.\n\n"
+MSG_UPDATE_ERROR += PACKAGE_URL
+
 
 def get_by_url(url, bz=""):
     """通过url获取更新信息
@@ -18,21 +21,25 @@ def get_by_url(url, bz=""):
         bz (str, optional): 备注. Defaults to "".
 
     Returns:
-        dict: 更新信息
+        dict/str/None: 更新信息，网络错误的None,无须理会，str为错误信息
     """
+
     try:
         request = requests.get(url, timeout=TIME_OUT)
         if request.status_code == 200:
             get_logger().info("%s update msg ok", bz)
-            return request.json()
+            data = request.json()
+            if "version" in data:
+                return data
+
+        return MSG_UPDATE_ERROR
     except requests.exceptions.ConnectTimeout as e:
         get_logger().error(e)
-        return {}
     except requests.exceptions.ProxyError as e:
         get_logger().error(e)
-        return {}
     except Exception as e:  # pylint: disable=W0718
         get_logger().error(e)
+
     return None
 
 
@@ -40,7 +47,7 @@ def get_by_gitee():
     """gitee
 
     Returns:
-        _type_: _description_
+        dict/str/None: 更新信息，网络错误的None,无须理会，str为错误信息
     """
     url = "https://gitee.com/yuhldr/lfy/raw/main/data/version.json"
 
@@ -51,7 +58,7 @@ def get_by_github():
     """github
 
     Returns:
-        _type_: _description_
+        dict/str/None: 更新信息，网络错误的None,无须理会，str为错误信息
     """
     url = "https://raw.githubusercontent.com/ldrfy/lfy/main/data/version.json"
 
@@ -92,20 +99,18 @@ def main():
     Returns:
         str: 更新信息或None
     """
-    s = "Please update. There is a problem with the current version configuration.\n\n"
-    s += PACKAGE_URL
     try:
         data = get_by_github()
-        if data is None or "version" not in data:
+        if not isinstance(data, dict):
             data = get_by_gitee()
-        if data is None or "version" not in data:
-            return s
+
+        if not isinstance(data, dict):
+            return data
 
         v = data["version"]
         if compare_versions(v, VERSION):
             return f'New version: {v}\n\nplease upgrade it:\n{data["url"]}\n\n{data["msg"]}'
         return None
-    # pylint: disable=W0718
-    except Exception as e:
+    except Exception as e:  # pylint: disable=W0718
         get_logger().error(e)
-        return s
+        return MSG_UPDATE_ERROR
